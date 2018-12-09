f2ec2b1012698afdcbc227e37e95d555f5675ce7
Seth Hinz <sethhinz@me.com>
2018-12-09 13:19:05 -0800

    Can serve resources now, horribly insecure and should never be run publically

--- src/server.cr
+++ src/server.cr
@@ -1,7 +1,38 @@
 require "http/server"
 
-module Server
-  VERSION = "0.1.0"
+server = HTTP::Server.new do |context|
+  context.response.content_type = "text/html"
+  
+  serve(context.response, context.request.path)
 
-  # TODO: Put your code here
+  puts "#{context.request.method} request:"
+  puts "Headers:"
+  context.request.headers.pretty_print PrettyPrint.new(STDOUT)
+  puts "Body #{context.request.body}"
+  puts "Path: #{context.request.path}"
+  puts
 end
+
+def serve(io : IO, path)
+  prefix = "src"
+  index = "index.html"
+  not_found = "404.html"
+  
+  case path
+  when "/"
+    path = "#{prefix}/#{index}"
+  else
+    path = "#{prefix}#{path}"
+  end
+
+  puts "seeking resource #{path}"
+
+  if File.exists? path
+    io.print File.read(path)
+  else
+    io.print File.read("#{prefix}/#{not_found}")
+  end
+end
+
+puts "Running server on localhost:8080"
+server.listen(8080)
